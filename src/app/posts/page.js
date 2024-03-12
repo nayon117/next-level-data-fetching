b0cfe1b2b3d008c583ee963583a9c60a40b13f51
nayon117 <hasibul.nayon1@gmail.com>
2024-03-12 16:01:16 +0600

    learn about revalidate

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,23 +1,26 @@
 const PostsPage = async () => {
-  const res = await fetch("http://localhost:5000/posts");
+  const res = await fetch("http://localhost:5000/posts",{
+     next:{
+        revalidate:5
+     }
+  });
   const posts = await res.json();
   console.log(posts);
   return (
-    <div>
+    <div className="container px-4 md:px-8 lg:px-16 ">
       <h1 className="text-3xl text-center p-2">
-        {" "}
-        Total Post : {posts.length}{" "}
+        Total Posts : {posts.length} 
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {posts?.map((post) => (
           <div key={post.id}>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-              
+            <div className="card card-compact my-5  bg-gray-100 shadow-xl">
               <div className="card-body">
                 <h2 className="card-title">{post.title}</h2>
                 <p>{post.description}</p>
+                <p>Likes: {post.likes_count}</p>
                 <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Read More</button>
+                  <button className="btn btn-primary">see More</button>
                 </div>
               </div>
             </div>
